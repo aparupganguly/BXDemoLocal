6e643046f988b9f5385a725984a903ef0999dd83
Aparup Ganguly <aparupganguly86@gmail.com>
2024-06-29 16:26:59 +0530

    login route

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PageNotFound from "./Components/PageNotFound";
 import Box1 from "./Components/Box1";
 import Box2 from "./Components/Box2";
+import FreelancerExplore from "./Components/FreelancerExplore";
+import HirerExplore from "./Components/HirerExplore";
 import BoxSelection from "./Components/BoxSelection";
+import LoginPage from "./Components/LoginPage";
 import "./index.css";
 import App from "./App";
 
@@ -18,6 +21,10 @@ root.render(
         <Route path="/" element={<BoxSelection />} />
         <Route path="/hiring-dashboard" element={<Box1 />} />
         <Route path="/freelance-dashboard" element={<Box2 />} />
+        <Route path="/hirer-explore" element={<HirerExplore />} />
+        <Route path="/freelancer-explore" element={<FreelancerExplore />} />
+        <Route path="/login" element={<LoginPage />} />
+
       </Routes>
     </Router>
   </>
