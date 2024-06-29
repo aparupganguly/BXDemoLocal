3dd0399d3459458c3f568f053cbc345a3e773ed5
NachiketaTiwari <nachiketatiwari56@gmail.com>
2024-06-29 17:14:28 +0530

    login Page done

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React from 'react'
+import {Link} from "react-router-dom";
+import "../Components/LoginPage.css"
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
+  return (
+    <>
+      <div className='Formpage'>
+        <h1 className="header-Text">BountyX</h1>
+        <div className="login-container">
+          <p className='Header'>Login for the Best Experience</p>
+          <div className="LoginInput">
+            <input className="inputbox" type="email" placeholder="Email" required />
+            <input className="inputbox" type="password" placeholder="Password" required />
+            <p className='altoption'>Dont have an Account?  
+              <Link to ="/">
+                <span className='gotosignup'>  SIGN UP</span>
+              </Link>
+            </p>
+            <button className='login-btn'>Login</button>
+          </div>
+        </div>
+      </div >
+    </>
+  )
+}
 
-export default LoginPage;
+export default LoginPage
