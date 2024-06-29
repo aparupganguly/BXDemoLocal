87bcdfa358128fc3f606805302f17b450f594a8d
Aparup Ganguly <aparupganguly86@gmail.com>
2024-06-29 19:40:58 +0530

    Developed Freelance Explore

--- src/Components/FreelancerExplore.js
+++ src/Components/FreelancerExplore.js
@@ -1,5 +1,13 @@
 import React from "react";
 import "./FreelancerExplore.css";
+
+// import img1 from "../assets/images/img1.png";
+// import img2 from "../assets/images/img2.png";
+// import img3 from "../assets/images/img3.png";
+// import img4 from "../assets/images/img4.png";
+// import img5 from "../assets/images/img5.png";
+// import img6 from "../assets/images/img6.png";
+
 const FreelancerExplore = () => {
   return (
     <div className="freelancerExploreContainer">
@@ -13,14 +21,21 @@ const FreelancerExplore = () => {
         </div>
       </div>
       <div class="Feedcontainer">
-        <div className="buttons">Post Gig/JOB </div>
+        <div className="buttons">
+          Post Gig
+        </div>
         <div className="Cards">
-          <div className="card1">Card 1</div>
-          <div className="card2">Card 2</div>
-          <div className="card3">Card 3</div>
-          <div className="card4">Card 4</div>
-          <div className="card5"> Card 5</div>
-          <div className="card6">Card 6</div>
+          <div className="card">
+            {/* <img src={img1} alt="" /> */}
+            <p>Looking for Modern and Trendy Designer</p>
+            <p style={{ fontSize: "15px" }}>User Name</p>
+            <p style={{ fontSize: "18px" }}> 5 ETH</p>
+          </div>
+          <div className="card">Card 2</div>
+          <div className="card">Card 3</div>
+          <div className="card">Card 4</div>
+          <div className="card"> Card 5</div>
+          <div className="card">Card 6</div>
         </div>
       </div>
     </div>
